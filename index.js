6a8f98e7577cd1c4a3ff84d0f563c1434487611a
Nasir Saeed <92154367+Nasir-Saeed@users.noreply.github.com>
2024-03-17 23:21:02 +0500

    Adding Files

---
+++ index.js
@@ -0,0 +1,24 @@
+// Arithmetic Operator in Javascript
+
+// In this video we will see five Arithmetic Operators
+
+// 1: Addition +
+// 2: Subtraction -
+// 3: Multiplication: *
+// 4: Division: /
+// 5: Modulus: %
+
+
+let a = 20;
+let b = 24;
+
+console.log("The + of two numbers: ", a + b); // 20 + 24
+console.log("The - of two numbers: ", a - b); // 20 - 24
+console.log("The * of two numbers: ", a * b); // 20 * 24
+console.log("The / of two numbers: ", a / b); // 20 / 24
+console.log("The % of two numbers: ", a % b); // 20 % 24
+
+console.log("Thanks for Watching Subscribe my Youtube Channel..!");
+
+
+// Source code link in the video description
